b5a55b2eb346a3b7ba929c25dd476a2f3a572103
reddammap <92972841+reddammap@users.noreply.github.com>
2021-10-22 12:39:01 +0530

    Create display.py

---
+++ display.py
@@ -0,0 +1,9 @@
+list-1 = [10,20,30,40]
+res_list = [x^2 for x in list-1]
+print(res_list)
+
+list-2 = [101,202,303,404,505]
+sub_list = [x*0.1 for x in list-2]
+print(sub_list)
+
+print("---The List is Completed----")
